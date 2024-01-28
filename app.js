a958c09a629bd449a644887c97a5c25f4b927f41
Brize Glace <viktorovitchromain@gmail.com>
2024-01-28 02:01:25 +0100

    try another thing....

--- app.js
+++ app.js
@@ -82,8 +82,8 @@ MicroModal.init({
 window.addEventListener('resize', setHeight);
 window.addEventListener('load', setHeight);
 
-function setHeight() {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 document.getElementById("currentYear").innerText = new Date().getFullYear();
