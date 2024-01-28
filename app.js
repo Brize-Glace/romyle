75ac63a1220e9c20d425720220aef0aba05cdc72
Brize Glace <viktorovitchromain@gmail.com>
2024-01-28 02:11:23 +0100

    uilefvh

--- app.js
+++ app.js
@@ -82,8 +82,6 @@ MicroModal.init({
 window.addEventListener('resize', setHeight);
 window.addEventListener('load', setHeight);
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.getElementById("background").style.height = window.innerHeight + 'px';
+
 document.getElementById("currentYear").innerText = new Date().getFullYear();
