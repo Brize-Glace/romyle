58d03a8a99d96ff50352283affbf49a147dcfc62
Brize Glace <viktorovitchromain@gmail.com>
2024-02-13 21:32:28 +0100

    Update countdown display logic and add new music icons

--- countdown.js
+++ countdown.js
@@ -9,11 +9,22 @@ const countdown = setInterval(function () {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById('countdown').innerText = `${days} days ${hours} hours ${minutes} minutes and ${seconds} seconds`;
+    if (days == 0) {
+        document.getElementById('countdown').innerText = `${hours} hours ${minutes} minutes and ${seconds} seconds`; 
+        return;
+    } if (hours == 0 && days == 0) {
+        document.getElementById('countdown').innerText = `${minutes} minutes and ${seconds} seconds`;  
+    } else {
+        document.getElementById('countdown').innerText = `${days} days ${hours} hours ${minutes} minutes and ${seconds} seconds`;
+    }
+
+    
 
     if (distance < 0) {
         clearInterval(countdown);
         document.getElementById('countdown').style.display = 'none';
         document.getElementById('icon-container').style.display = 'flex';
+        document.getElementById('preOrder').style.display = 'none';
+        document.getElementsByTagName('iframe')[0].style.display = 'none';
     }
 }, 1000);
